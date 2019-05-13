969255b626c5a4ceb765505be4d2c8b2b3df9fc4
Christopher M. Biwer <christopher.biwer@ligo.org>
2019-05-13 17:28:47 -0400

    Add pp.

---
+++ pycbc_inference_workflow/run_ex_pp.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+set -e
+
+WORKFLOW_NAME=emcee_pt_pptest
+OUTPUT_DIR=run
+
+export OMP_NUM_THREADS=1
+
+pycbc_make_inference_inj_workflow \
+    --output-dir ${OUTPUT_DIR} \
+    --workflow-name ${WORKFLOW_NAME} \
+    --data-type simulated_data \
+    --output-file ${WORKFLOW_NAME}.dax \
+    --inference-config-file ex_pp_inf.ini \
+    --config-files ex_pp_wf.ini \
+    --config-overrides results_page:output-path:${PWD}/${OUTPUT_DIR}/results_html
+
+cd ${OUTPUT_DIR}
+pycbc_submit_dax \
+   --no-create-proxy \
+   --no-grid \
+   --dax ${WORKFLOW_NAME}.dax \
+   --enable-shared-filesystem
