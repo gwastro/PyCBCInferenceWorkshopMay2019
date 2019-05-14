9881e8ff024a923015e1139011360a9e8eb90c22
Christopher M. Biwer <cmbiwer@gmail.com>
2019-05-14 11:43:50 +0100

    Comments.

--- pycbc_inference_workflow/run_ex_pp.sh
+++ pycbc_inference_workflow/run_ex_pp.sh
@@ -7,6 +7,7 @@ OUTPUT_DIR=pp
 
 export OMP_NUM_THREADS=1
 
+# run workflow generator
 pycbc_make_inference_inj_workflow \
     --output-dir ${OUTPUT_DIR} \
     --workflow-name ${WORKFLOW_NAME} \
@@ -16,10 +17,11 @@ pycbc_make_inference_inj_workflow \
     --config-files ex_pp_wf.ini \
     --config-overrides results_page:output-path:${PWD}/${OUTPUT_DIR}/results_html
 
-cd ${OUTPUT_DIR}
-pycbc_submit_dax \
-   --no-create-proxy \
-   --no-grid \
-   --dax ${WORKFLOW_NAME}.dax \
-   --enable-shared-filesystem \
-   --force-no-accounting-group
+# execute workflow
+#cd ${OUTPUT_DIR}
+#pycbc_submit_dax \
+#   --no-create-proxy \
+#   --no-grid \
+#   --dax ${WORKFLOW_NAME}.dax \
+#   --enable-shared-filesystem \
+#   --force-no-accounting-group
